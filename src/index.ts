0452d6a11195fec0e3b40cddc4c55be42f1c8195
osyrisrblx <osyrisrblx@gmail.com>
2021-10-27 18:42:39 -0700

    Update comments, add `public` modifiers

--- src/index.ts
+++ src/index.ts
@@ -9,23 +9,23 @@ export class Bin {
 	private items = new Array<Bin.Item>();
 
 	/**
-	 * Adds an item into the bin. This can be a:
+	 * Adds an item into the Bin. This can be a:
 	 * - `() => unknown`
 	 * - RBXScriptConnection
 	 * - Object with `.destroy()` or `.Destroy()`
 	 */
-	add<T extends Bin.Item>(item: T): T {
+	public add<T extends Bin.Item>(item: T): T {
 		this.items.push(item);
 		return item;
 	}
 
 	/**
-	 * Destroys all items currently in the bin:
+	 * Destroys all items currently in the Bin:
 	 * - Functions will be called
 	 * - RBXScriptConnections will be disconnected
 	 * - Objects will be `.destroy()`-ed
 	 */
-	destroy() {
+	public destroy(): void {
 		const items = this.items;
 		this.items = [];
 		for (const item of items) {
