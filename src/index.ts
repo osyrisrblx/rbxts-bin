9eeb80eaba84b9c3bae37312eabdb22816779a7d
osyrisrblx <osyrisrblx@gmail.com>
2021-10-27 18:32:14 -0700

    initial commit :wastebasket:

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+export namespace Bin {
+	export type Item = (() => unknown) | RBXScriptConnection | { destroy(): void } | { Destroy(): void };
+}
+
+/**
+ * Tracks connections, instances, functions, and objects to be later destroyed.
+ */
+export class Bin {
+	private items = new Array<Bin.Item>();
+
+	/**
+	 * Adds an item into the bin. This can be a:
+	 * - `() => unknown`
+	 * - RBXScriptConnection
+	 * - Object with `.destroy()` or `.Destroy()`
+	 */
+	add<T extends Bin.Item>(item: T): T {
+		this.items.push(item);
+		return item;
+	}
+
+	/**
+	 * Destroys all items currently in the bin:
+	 * - Functions will be called
+	 * - RBXScriptConnections will be disconnected
+	 * - Objects will be `.destroy()`-ed
+	 */
+	destroy() {
+		const items = this.items;
+		this.items = [];
+		for (const item of items) {
+			if (typeIs(item, "function")) {
+				item();
+			} else if (typeIs(item, "RBXScriptConnection")) {
+				item.Disconnect();
+			} else if ("destroy" in item) {
+				item.destroy();
+			} else if ("Destroy" in item) {
+				item.Destroy();
+			}
+		}
+	}
+}
